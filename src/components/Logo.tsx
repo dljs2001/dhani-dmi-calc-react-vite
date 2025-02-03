23591b548107ad9172adb47c7b67b256c34b593a
dljs2001 <dljs9899@gmail.com>
2025-02-03 13:25:18 +0530

    Logo saved

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export function Logo() {
   return (
     <img 
-      src="https://imgur.com/4Rj60nI" 
+      src="https://i.imgur.com/4Rj60nI.png" 
       alt="Company Logo" 
       className="h-16 object-contain"
     />
